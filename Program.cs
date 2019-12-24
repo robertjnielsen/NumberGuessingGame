f288eca4d63242a4188656f56b54f628485a8a59
Robert James Nielsen <robert.j.nielsen@outlook.com>
2019-12-23 19:07:51 -0800

    App header info.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
+// Namespace
 namespace NumberGuessingGame
 {
+    // Main Class
     class Program
     {
+        // Entry Point Method
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Set up application variables.
+            string appName = "Number Guessing Game";
+            string appVersion = "1.0.0";
+            string appAuthor = "Robert Nielsen";
+
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
         }
     }
 }
